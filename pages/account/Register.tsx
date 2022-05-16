5b8045081c938ba4626035dbbc1ce7765dbbd699
git hub user name <kotieheim@gmail.com>
2022-05-16 19:22:21 -0400

    Setting up registration form

---
+++ pages/account/Register.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Layout from "../../components/account/AccountLayout";
+import RegistrationInput from "../../components/account/RegistrationInput";
+import { registrationMapper } from "../../services/accountMapper";
+
+type Props = {};
+
+const Register = (props: Props) => {
+  return (
+    <Layout title="Register Here">
+      <div className="register-container">
+        <h4 className="register-header">Register</h4>
+        <div className="register-body">
+          <form action="submit">
+            {registrationMapper.map((item, index) => (
+              <div key={index}>
+                <RegistrationInput
+                  labelText={item.label}
+                  inputName={item.input}
+                />
+              </div>
+            ))}
+          </form>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Register;
---
+++ services/accountMapper.ts
@@ -0,0 +1,22 @@
+export const registrationMapper = [
+  {
+    label: "First Name",
+    input: "firstName",
+    type: "text",
+  },
+  {
+    label: "Last Name",
+    input: "lastName",
+    type: "text",
+  },
+  {
+    label: "Username",
+    input: "username",
+    type: "text",
+  },
+  {
+    label: "Password",
+    input: "password",
+    type: "password",
+  },
+];
